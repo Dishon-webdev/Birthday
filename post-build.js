ea2fb555ccf126e3b80cc4ba38050e1e181bf661
Dishon. C <consalvesdishon16@gmail.com>
2026-04-30 11:31:08 +0530

    Remove _redirects file to fix Cloudflare deployment

--- post-build.js
+++ post-build.js
@@ -7,12 +7,8 @@ const __dirname = path.dirname(__filename);
 
 const distDir = path.join(__dirname, "dist");
 
-// Create _redirects file
-const redirectsContent = "/* /Birthday/index.html 200\n";
-fs.writeFileSync(path.join(distDir, "_redirects"), redirectsContent);
-
-// Create _headers file
+// Create _headers file for proper MIME type handling
 const headersContent = "/*\n  X-Content-Type-Options: nosniff\n";
 fs.writeFileSync(path.join(distDir, "_headers"), headersContent);
 
-console.log("Created _redirects and _headers files in dist/");
+console.log("Created _headers file in dist/");
